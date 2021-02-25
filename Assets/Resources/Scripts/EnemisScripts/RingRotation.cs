365b46a3628db514059b8c817fbe549ffa5016ef
Shahari93 <shaharilani93@gmail.com>
2021-02-25 17:59:01 +0200

    Orginazing folder and animation fixes

---
+++ Assets/Resources/Scripts/EnemisScripts/RingRotation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingRotation : MonoBehaviour
+{
+    [SerializeField] private float ringRotationSpeed = 0f;
+    void Update()
+    {
+        transform.Rotate(Vector3.left * Time.deltaTime * ringRotationSpeed);
+    }
+}
