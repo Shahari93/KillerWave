02a765ec81f983d44f8e116bf21039a90d2a69c2
Shahari93 <shaharilani93@gmail.com>
2021-02-25 17:45:47 +0200

    Animation fixes

---
+++ Assets/Resources/Scripts/EnemyScripts/BasicEnemyRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemyRotate : MonoBehaviour
+{
+    [SerializeField] private float rotateSpeed = 0; 
+
+    void Update()
+    {
+        transform.Rotate(Vector3.left * Time.deltaTime * rotateSpeed);
+    }
+}
