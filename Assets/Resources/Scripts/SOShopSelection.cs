de43b16df78b04ca526ef625ff1d75e28c3e8baf
Shahari93 <shaharilani93@gmail.com>
2021-02-26 10:01:31 +0200

    Updated shop

---
+++ Assets/Resources/Scripts/SOShopSelection.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(menuName = "Shop", fileName = "Shop Selection")]
+public class SOShopSelection : ScriptableObject
+{
+    public Sprite icon;
+    public string iconName;
+    public string iconDescription;
+    public string itemCost;
+}
