02565983c177f5386849f39854163ab6be120dba
Shahari93 <shaharilani93@gmail.com>
2021-04-05 00:02:53 +0300

    Remote config

---
+++ Assets/Resources/Scripts/Managers/RemoteSettingsStartup.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+//public class RemoteSettingsStartup : MonoBehaviour
+//{
+//    private void Awake()
+//    {
+//        if(Application.internetReachability == NetworkReachability.ReachableViaLocalAreaNetwork || Application.internetReachability == NetworkReachability.ReachableViaCarrierDataNetwork) // checking if the player have internet
+//        {
+//            RemoteSettings.Updated += () =>
+//             {
+//                 GameManager.playerLives = RemoteSettings.GetInt("PlayerStartUpLives", GameManager.playerLives);
+//             };
+//        }
+//    }
+//}
--- Assets/Resources/Scripts/TitleComponent.cs
+++ Assets/Resources/Scripts/TitleComponent.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 // Load the shop scene, and set the player lives to 3
 public class TitleComponent : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // No need for this because now we set the amount of lives to X depents if the player is conected to the internet
     void Start()
     {
         GameManager.playerLives = 3;
