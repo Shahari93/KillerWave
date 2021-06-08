0d6f37514743bcb3b5ec3319a11143c796b6cbaf
Shahari93 <shaharilani93@gmail.com>
2021-06-08 22:51:27 +0300

    Fixes for mobile version
    New way to check screen sizes
    Fixes for canvas scailing
    Fixes for mobile lights

--- Assets/Resources/Scripts/PlayerScripts/Player.cs
+++ Assets/Resources/Scripts/PlayerScripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class Player : MonoBehaviour, IActorTemplate
 {
@@ -12,11 +13,12 @@ public class Player : MonoBehaviour, IActorTemplate
     Vector3 direction; //will hold the player's touch screen location
     Rigidbody rb;
     public static bool mobile = false;
+    GameObject[] screenPoints = new GameObject[2];
     //The last two variables of width and height will be used to store the
     //measured results of the world space dimensions of the screen the game is
     //played in.
-    float height;
-    float width;
+    //float height;
+    //float width;
 
 
     float camTravelSpeed;
@@ -68,11 +70,34 @@ mobile = true;
 InvokeRepeating("Attack",0,0.3f);
 rb = GetComponent<Rigidbody>();
 #endif
-        //viewport space is similar to what we know as a screen resolution, except its measurements, are in points and not pixels, and these points are measured from 0 to 1.
-        height = 1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f); // WorldToViewportPoint method take the results from the game's three-dimensional world space and convert the results into viewport space
-        width = 1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0)).x - .5f); // This will give us our current world space width of the screen.
         _Player = GameObject.Find("_Player");
-        moveingScreen = width;
+        CalculateBoundaries();
+        //viewport space is similar to what we know as a screen resolution, except its measurements, are in points and not pixels, and these points are measured from 0 to 1.
+        //height = 1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0)).y - .5f); // WorldToViewportPoint method take the results from the game's three-dimensional world space and convert the results into viewport space
+        //width = 1 / (Camera.main.WorldToViewportPoint(new Vector3(1, 1, 0)).x - .5f); // This will give us our current world space width of the screen.
+        //moveingScreen = width;
+    }
+
+    private void CalculateBoundaries()
+    {
+        // Creates 2 new gameobjects named p1 and p2
+        screenPoints[0] = new GameObject("p1");
+        screenPoints[1] = new GameObject("p2");
+
+        //ViewportToWorldPoint give us our game world space position
+        Vector3 v1 = Camera.main.ViewportToWorldPoint(new Vector3(0, 1, 300));
+        Vector3 v2 = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, 300));
+
+        //apply our new Vector3 variables, v1 and v2, to our array of game object's positions
+        screenPoints[0].transform.position = v1;
+        screenPoints[1].transform.position = v2;
+
+        //need to make them children of the Player script, which will update their Transform Position values.
+        screenPoints[0].transform.SetParent(this.transform.parent);
+        screenPoints[1].transform.SetParent(this.transform.parent);
+
+        //we update the movingScreen float value with our screenPoint value for when the game has a moving camera.
+        moveingScreen = screenPoints[1].transform.position.x;
     }
 
     private void Update()
@@ -95,7 +120,8 @@ rb = GetComponent<Rigidbody>();
 
     private void MobileControl()
     {
-        if (Input.touchCount > 0)
+        // Checking if there is a touch on screen, and that we didn't touched on any of UI stuff
+        if (Input.touchCount > 0 && EventSystem.current.currentSelectedGameObject == null)
         {
             Touch touch = Input.GetTouch(0);
             Vector3 touchPos = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, 300));
@@ -103,7 +129,7 @@ rb = GetComponent<Rigidbody>();
             direction = (touchPos - transform.position);
             rb.velocity = new Vector3(direction.x, direction.y, 0) * 5;
             direction.x += moveingScreen;
-            if(touch.phase == TouchPhase.Ended)
+            if (touch.phase == TouchPhase.Ended)
             {
                 rb.velocity = Vector3.zero;
             }
@@ -118,6 +144,8 @@ rb = GetComponent<Rigidbody>();
             transform.position += Vector3.right * camTravelSpeed * Time.deltaTime;
             moveingScreen += Time.deltaTime * camTravelSpeed;
         }
+        else
+            moveingScreen = 0;
     }
 
     private void Attack()
@@ -136,7 +164,7 @@ rb = GetComponent<Rigidbody>();
         float verMove = Input.GetAxisRaw("Vertical");
         if (horMove > 0)
         {
-            if (transform.localPosition.x < moveingScreen + (width / 0.9f))
+            if (transform.localPosition.x < (screenPoints[1].transform.localPosition.x - screenPoints[1].transform.localPosition.x / 30f) + moveingScreen)
             {
                 transform.localPosition += Vector3.Normalize(new Vector3(horMove * Time.deltaTime * travelSpeed, 0, 0));
             }
@@ -144,7 +172,7 @@ rb = GetComponent<Rigidbody>();
 
         if (horMove < 0)
         {
-            if (transform.localPosition.x > moveingScreen + width / 6f)
+            if (transform.localPosition.x > (screenPoints[1].transform.localPosition.x - screenPoints[1].transform.localPosition.x / 30) + moveingScreen)
             {
                 transform.localPosition += Vector3.Normalize(new Vector3(horMove * Time.deltaTime * travelSpeed, 0, 0));
             }
@@ -152,7 +180,7 @@ rb = GetComponent<Rigidbody>();
 
         if (verMove < 0)
         {
-            if (transform.localPosition.y > -height / 3f)
+            if (transform.localPosition.y > (screenPoints[1].transform.localPosition.y - screenPoints[1].transform.localPosition.y / 3f))
             {
                 transform.localPosition += Vector3.Normalize(new Vector3(0, verMove * Time.deltaTime * travelSpeed, 0));
             }
@@ -160,7 +188,7 @@ rb = GetComponent<Rigidbody>();
 
         if (verMove > 0)
         {
-            if (transform.localPosition.y < height / 2.5f)
+            if (transform.localPosition.y < (screenPoints[0].transform.localPosition.y - screenPoints[0].transform.localPosition.y / 5f))
             {
                 transform.localPosition += Vector3.Normalize(new Vector3(0, verMove * Time.deltaTime * travelSpeed, 0));
             }
